3c59fb1cab61a140947a829cf47a77b148d53bdc
ILLYA SMYK <smykillya@gmail.com>
2025-08-09 23:56:34 +0200

    2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "./CafeInfo/CafeInfo";
-import VoteOptions from "./VoteOptions/VoteOptions";
-import VoteStats from "./VoteStats/VoteStats";
-import Notification from "./Notification/Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
-import type { Votes, VoteType } from "../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 
 function App() {
   // Создаю состояние для голосов с начальными значениями: обьект
